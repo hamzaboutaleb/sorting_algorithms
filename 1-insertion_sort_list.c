2efed57657c2c01723b16bb4fda31f313e1e1424
hamzaboutaleb <hamza-boutaleb@live.fr>
2023-06-23 12:55:46 +0100

    check for null list

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -37,7 +37,7 @@ void insertion_sort_list(listint_t **list)
 	listint_t *p1;
 	listint_t *p2;
 
-	if (*list == NULL)
+	if (*list == NULL || list == NULL)
 		return;
 	p1 = (*list)->next;
 	while (p1 != NULL)
