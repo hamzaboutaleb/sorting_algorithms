5058679bb5b72f615d64047a59222808568a33e1
hamzaboutaleb <hamza-boutaleb@live.fr>
2023-06-23 12:12:19 +0100

    inserion sort

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,60 @@
+#include "sort.h"
+#include <stdio.h>
+
+/**
+ * swap - swap to elemnt
+ * @prevList: first list
+ * @nextList: second list
+*/
+void swap(listint_t *prevList, listint_t *nextList)
+{
+	listint_t *temp1 = NULL, *temp2 = NULL;
+
+	if (nextList->next)
+		temp1 = nextList->next;
+	if (prevList->prev)
+		temp2 = prevList->prev;
+
+	prevList->next = temp1;
+	nextList->prev = temp2;
+
+	prevList->prev = nextList;
+	nextList->next = prevList;
+
+	if (temp1)
+		temp1->prev = prevList;
+	if (temp2)
+		temp2->next = nextList;
+}
+
+/**
+ * insertion_sort_list - sort list
+ * @list: pointer to list head
+*/
+
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *p1;
+	listint_t *p2;
+
+	p1 = (*list);
+	while (p1 != NULL)
+	{
+		p2 = p1;
+		p1 = p1->next;
+		while (p2 && p2->prev)
+		{
+			if (p2->n < p2->prev->n)
+			{
+				swap(p2->prev, p2);
+				if (p2->prev == NULL)
+					(*list) = p2;
+				print_list(*list);
+			}
+			else
+			{
+				p2 = p2->next;
+			}
+		}
+	}
+}
--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 ## Bubble sort
 Bubble sort is a simple sorting algorithm that repeatedly compares adjacent elements and swaps them if they are in the wrong order. It continues this process until the list is sorted.
+
+## Insertion sort
+Insertion sort is a simple algorithm that iterates through an array, comparing each element with the ones before it and placing it in the correct position. It builds a sorted section gradually and is efficient for small arrays or partially sorted data
